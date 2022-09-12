7f855d56a3bcb3323c64a67c0e7dac63c52bf45f
Adarsh VD <32849302+adarshvd@users.noreply.github.com>
2022-09-12 13:10:52 +0530

    Update palindromewith recursion.py

--- palindromewith recursion.py
+++ palindromewith recursion.py
@@ -11,6 +11,11 @@ def recu(i):
         return 1
     return 1*recu(i-1)
 
+def pal(string):  # AVD
+    if len(string)<=1:
+        return True
+    return (string[0]==string[-1]) and pal(string[1:-1])
+
 input_string="madam"
 l=len(input_string)-1
 flag=recu(l)
